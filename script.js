049ac0547aac341e1215c7917d3313749617ab77
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-06-25 16:05:34 +0200

    [UPD] Defined two important starting variables

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Definisco la distanza percorsa durante il viaggio
+
+let travel_distance = prompt("Quanti chilometri percorrerà il treno?");
+console.log(travel_distance);
+
+
+// Definisco il prezzo del biglietto senza applicare sconti
+
+let ticket_price = 0.21 * travel_distance;
+console.log(ticket_price);
